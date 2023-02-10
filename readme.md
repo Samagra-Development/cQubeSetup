48a4994f4782f366c5461fffd2daad7f12f68fcf
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2023-02-10 12:09:44 +0530

    feat: added flag to choose between node and yarn

--- readme.md
+++ readme.md
@@ -3,4 +3,5 @@
 ```
 curl -s -L https://raw.githubusercontent.com/Samagra-Development/cQubeSetup/main/run-server.sh  | bash  -s -- -p PORT1 -q PORT2
 ```
-replace PORT1 & PORT2 with the available ports on your system
+* replace PORT1 & PORT2 with the available ports on your system ,default is 3001 and 3002.
+* pass -n flag if you want to install dependencies using npm ,default is set to yarn
--- run-server.sh
+++ run-server.sh
@@ -6,11 +6,13 @@ cd cQube-POCs
 touch .env 
 TIMESCALEDB_PORT=3001
 PGPOOL_SERV_PORT=3002
+IS_YARN=true
 while getopts p:q: flag
 do
     case "${flag}" in
         p) TIMESCALEDB_PORT=${OPTARG};;
         q) PGPOOL_SERV_PORT=${OPTARG};;
+        n) IS_YARN=false;;
     esac
 done
 echo "TIMESCALEDB_PORT: $TIMESCALEDB_PORT";
@@ -19,6 +21,13 @@ echo "TIMESCALEDB_PORT="$TIMESCALEDB_PORT"" > .env && echo "PGPOOL_SERV_PORT="$P
 docker-compose up -d
 cd impl/c-qube
 touch .env && echo "DATABASE_URL="postgres://timescaledb:postgrespassword@localhost:$TIMESCALEDB_PORT/postgres?sslmode=disable"" > .env
-yarn install
-npx prisma migrate dev
-yarn test
+if [ $IS_YARN  -eq  true ]
+then
+  yarn install
+  npx prisma migrate dev
+  yarn test
+else
+  npm install
+  npx prisma migrate dev
+  npm run test
+fi
